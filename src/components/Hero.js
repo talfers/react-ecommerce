16f7f9fcd191ab78c492c4b9948a22fa21ad0714
Taylor Alfers <Taylor>
2019-08-21 18:13:38 -0600

    spelling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,7 +9,7 @@ function Hero({img}) {
       <img src={img} alt='hero-img'/>
       <div className="hero-content">
         <h1 className="hero-title text-alternate">Cake is Good.</h1>
-        <h3 className="hero-text text-alternate">Browse our many different cakes, both chocloately and fruity.</h3>
+        <h3 className="hero-text text-alternate">Browse our many different cakes, both chocolatey and fruity.</h3>
         <Link to="/products">
           <button className="hero-btn">Shop Now</button>
         </Link>
